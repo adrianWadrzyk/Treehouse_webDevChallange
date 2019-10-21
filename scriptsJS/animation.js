88e7b5a8020bfd76e402c0928f2fc65d0bbdc3ff
adrianWadrzyk <adrian.wadrzyk.contact@gmail.com>
2019-10-21 21:03:36 +0200

    Added animatons for sections

--- scriptsJS/animation.js
+++ scriptsJS/animation.js
@@ -1,4 +1,4 @@
-window.addEventListener("load", function() {
+window.addEventListener("load", () => {
     const ourServices = document.getElementById("ourServices");
     ourServices.style.animation = "showing 5s";
 
@@ -8,15 +8,43 @@ window.addEventListener("load", function() {
     const main = document.getElementById("main");
     var mainChildren = main.children;
 
-    for (let i = 0; i < mainChildren.length; i++) {
-        mainChildren[i].style.opacity = 0;
-    }
+    main.style.animation = "slide-left 2s";
 
-    main.style.animation = "slide-left 3s";
-
+    // show main section elements
     showChild = this.setTimeout(function() {
-        for (let i = 0; i < mainChildren.length; i++) {
-            mainChildren[i].style.opacity = 1;
+        for (const el of mainChildren) {
+            el.style.animation = "showing 3s forwards";
         }
-    }, 3200);
+    }, 2200);
+
+    const offer = document.getElementById("offer");
+    const cultureValueTiles = document.querySelectorAll(".culture__value > .tiles");
+
+    //scroll animation for sections
+    var onScrollHandler = () => {
+        let licznik = 0;
+        let clRectOffer = offer.getBoundingClientRect();
+        let clRectCultureValue = cultureValueTiles[0].parentNode.getBoundingClientRect();
+
+        if (clRectOffer.top < 550) {
+            offer.style.animation = "showing 3s forwards";
+            licznik++;
+        }
+
+        if (clRectCultureValue.top < 550) {
+            for (let el of cultureValueTiles) {
+                el.style.animation = " resize 3s forwards";
+            }
+            licznik++;
+        }
+        if (licznik == 2) {
+            window.removeEventListener('scroll', onScrollHandler);
+        }
+    };
+
+    window.addEventListener('scroll', onScrollHandler, false);
+
+
+
+
 });
